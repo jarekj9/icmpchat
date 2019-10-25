f0629da551aa7921be5ce0a3b22562b09145b735
jarekj9 <jerry.esp.sha@gmail.com>
2019-10-25 13:46:26 +0200

    v0.1

--- icmpchat.py
+++ icmpchat.py
@@ -13,17 +13,23 @@ class CHAT():
   def __init__(self,dstip,interface):
     self.dstip=dstip
     self.interface=interface
- 
+  
   def display_msg(self,pkt):
-      try:
+    try:
         print('')
         print('Received message: '+(pkt.load).decode())  
-      except:pass
-  def listen(self): 
-    while 1: 
-      sniff(iface=self.interface, prn=self.display_msg, filter="host "+self.dstip+" and icmp and icmp[0]=0", store=0, count=10)
-      sleep(1)
-      
+    except:pass
+  
+  #sniffing for icmp packets  
+  def listen(self):
+    try:
+        while 1: 
+            sniff(iface=self.interface, prn=self.display_msg, filter="host "+self.dstip+" and icmp and icmp[0]=0", store=0, count=10)
+    except:
+        print('\r\nError: Could not start sniffing for icmp packets on interface: '+self.interface)
+        os._exit(0)
+        
+  #accept input text and send it to peer 
   def receive_input(self): 
     while 1: 
       print('')
@@ -31,6 +37,18 @@ class CHAT():
       if text == 'exit()': os._exit(0)    
       MESSAGE(self.dstip).send_msg(text)
       sleep(0)
+      
+  #if interface is not specified, it tries to find it automatically  
+  def get_interface(self):
+    cmd = os.popen("ip r g "+self.dstip).read()
+    try: 
+        interface = cmd.split('dev ')[1].split(' ')[0]
+    except:
+        print('Failed to detect interface with command ip r g, using default eth0, maybe try to set it with -i flag')
+        interface = 'eth0'
+
+    return interface
+  
 
 #sending message
 class MESSAGE():
@@ -48,18 +66,24 @@ class MESSAGE():
 def main():
     parser = argparse.ArgumentParser(description='Please run as root and specify destination ip with -d flag')
     parser.add_argument('-d', type=str, help='Destination IP')
-    parser.add_argument('-i', type=str, default='eth0', help='Local interface name to send and listen, default is eth0')
+    parser.add_argument('-i', type=str, default=None, help='Local interface name to send and listen, by default it will try to find it automatically')
     args = parser.parse_args()
-    dstip = args.__dict__.get('d')
-    interface = args.__dict__.get('i')
     
-    try: 
-        print("Starting chat with "+dstip+' on '+interface+' ! Type exit() to exit')
-    except:
+    if args.d == None: 
         print('Please check options with -h flag')
         exit(0)
-        
     
+    dstip = args.__dict__.get('d')
+    if args.i: interface = args.__dict__.get('i')
+    else:
+        interface = CHAT(dstip,'').get_interface()
+    
+    
+    
+    print("Starting chat with "+dstip+' on '+interface+' ! Type exit() to exit')    
+    
+    #adding 2 threads: one for sniffing icmp, second for text input
+    CHAT(dstip,interface).get_interface()
     threads = []
     for func in [CHAT(dstip,interface).listen,CHAT(dstip,interface).receive_input]: 
         threads.append(Thread(target=func)) 
